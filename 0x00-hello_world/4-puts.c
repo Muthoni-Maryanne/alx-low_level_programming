c058bca8abb41b8a46bfe87ca4fad3049edca9fe
Maryanne_Muthoni <maryanne.muthoni@student.moringaschool.com>
2023-10-05 10:32:27 -0700

    script that compiles a C file and creates an executable named cisfun
    The C file name will be saved in the variable $CFILE

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
+/* File: 4-puts.c */
 #include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
