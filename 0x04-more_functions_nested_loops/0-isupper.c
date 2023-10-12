a8c45df6da81421256d8936aa46d366a575f8f9f
Maryanne_Muthoni <maryanne.muthoni@student.moringaschool.com>
2023-10-12 08:29:27 -0700

    function that checks for uppercase character.
    Prototype: int _isupper(int c);
    Returns 1 if c is uppercase
    Returns 0 otherwise

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,7 +4,7 @@
 
 /**
  * _isupper - Checks for uppercase characters.
- * c: The character to be checked.
+ * @c: The character to be checked.
  *
  * Return: 1 if character is uppercase, 0 otherwise.
  */
