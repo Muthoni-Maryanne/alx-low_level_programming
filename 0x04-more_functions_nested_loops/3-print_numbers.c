fcbc97f5cab4003af7db675666cbd1c0cafea450
Maryanne_Muthoni <maryanne.muthoni@student.moringaschool.com>
2023-10-12 10:06:21 -0700

    function that prints the numbers, from 0 to 9, followed by a new line
    Prototype: void print_numbers(void);
    You can only use _putchar twice in your code

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+/* File: 3-print_numbers.c */
+
+#include "main.h"
+
+/**
+ * print_numbers - Prints from 0-9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for(i=0; i<10; i++)
+	{
+		_putchar((i % 10) + '0');
+	}
+
+	_putchar('\n');
+}
