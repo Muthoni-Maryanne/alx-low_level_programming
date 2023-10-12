73c19d9b3315c7e827944a367775a94f29c381be
Maryanne_Muthoni <maryanne.muthoni@student.moringaschool.com>
2023-10-12 05:18:50 -0700

    function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
    Prototype: void jack_bauer(void);

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,9 +3,10 @@
 #include "main.h"
 
 /**
- * prints every minute of the day of Jack Bauer
+ * jack_bauer()- prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
  */
+
 void jack_bauer(void)
 {
 	int x, y;
