0d4a01fc91d034a38d8634bb87376f80a80a6f5a
Maryanne Muthoni <107298263+Muthoni-Maryanne@users.noreply.github.com>
2023-10-16 10:28:16 +0300

    Update 0-positive_or_negative.c
    Complete the source code in order to print whether the number stored in the variable n is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
 
 /**
  * main - Prints a random number and states whether
- *        it is positive, negative, or zero.
+ *        it is positive, is negative, or is zero.
  * Return: Always 0.
 */
 int main(void)
@@ -17,11 +17,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else if (n < 0)
+	{
 		printf("%d is negative\n", n);
+	}
 	else
+	{
 		printf("%d is zero\n", n);
-
+	}
+	
 	return (0);
 }
