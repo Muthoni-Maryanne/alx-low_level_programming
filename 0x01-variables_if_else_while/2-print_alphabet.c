2ab651bbd255ea35770e662f5e8cfe087cd9a217
Maryanne_Muthoni <maryanne.muthoni@student.moringaschool.com>
2023-10-16 02:56:35 -0700

    program that prints the alphabet in lowercase, followed by a new line.
    You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
    All your code should be in the main function
    You can only use putchar twice in your code
    Fix to Betty styling

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,12 +12,12 @@
  */
 int main(void)
 {
-        char alphabet;
+	char alphabet;
 
-        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-        {
-                putchar(alphabet);
-        }
-        putchar('\n');
-        return (0);
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
 }
