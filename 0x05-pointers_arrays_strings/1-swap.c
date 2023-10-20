2f5fc1f756bfd156edb4521b8ac74d7427453e62
Maryanne_Muthoni <maryanne.muthoni@student.moringaschool.com>
2023-10-19 19:30:13 -0700

    function that concatenates two strings.
    Prototype: char *_strncat(char *dest, char *src, int n);
    The _strncat function is similar to the _strcat function, except that
    it will use at most n bytes from src; and
    src does not need to be null-terminated if it contains n or more bytes
    Return a pointer to the resulting string dest

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,20 +9,20 @@
  */
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+ *a = 42;
+ *b = 98;
 }
 
 int main(void)
 {
-	int a = 98;
-	int b = 42;
-	int *p1;
-	int *p2;
+ int a = 98;
+ int b = 42;
+ int *p1;
+ int *p2;
 
-	p1 = &a;
-	p2 = &b;
-	swap_int(p1, p2);
+ p1 = &a;
+ p2 = &b;
+ swap_int(p1, p2);
 
-	return (0);
+ return (0);
 }
