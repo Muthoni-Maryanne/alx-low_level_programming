9232580950b21bd1286f3021789eceb796627653
Maryanne_Muthoni <maryanne.muthoni@student.moringaschool.com>
2023-10-24 02:48:17 -0700

    function that prints the alphabet, in lowercase, followed by a new line
    Prototype: void print_alphabet(void);
    You can only use _putchar twice in your code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet, in lowercase, followed by a new line
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  *
  * Return: void
  */
