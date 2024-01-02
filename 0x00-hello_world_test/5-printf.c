70a277d17e1dbc7c1f7a7c067438e614a8f0b22e
Maryanne Muthoni <maryanne.muthoni@student.moringaschool.com>
2024-01-01 22:15:29 -0800

    Fix bug

---
+++ 0x00-hello_world_test/5-printf.c
@@ -0,0 +1,14 @@
+/* File: 5-printf.c */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
