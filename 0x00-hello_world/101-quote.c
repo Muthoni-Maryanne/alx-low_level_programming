49a6ed5d93312c14e760f772fc0085d875d95cf5
Maryanne Muthoni <maryanne.muthoni@student.moringaschool.com>
2024-01-02 01:43:23 -0800

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
    You are not allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
    Your program should return 1
    Your program should compile without any warnings when using the -Wall gcc option
    syntax of write is:
    ssize_t write(int fd, const void buf[.count], size_t count);
    fd id the file desvcriptor which in this case 2 is 2 for standard error
    second parameter is the buffer which holds what we want to output to
    standard error
    Third paremeter is the size which in this case is 59 bytes/chars

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+/*File: 101-quote.c*/
+
+#include <unistd.h>
+
+/**
+ *main -prints '"and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *followed by a new line
+ * do not use printf, puts, putchar
+ *
+ *Return: 1
+ */
+int main(void)
+{
+	char *buffer = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2,buffer,59);
+
+	return (1);
+}
