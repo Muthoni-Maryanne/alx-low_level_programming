f16231214ef44e165460cbd2c1ed0c2324821ec9
Maryanne Muthoni <maryanne.muthoni@student.moringaschool.com>
2024-01-03 03:27:00 -0800

    The output of the program should be:
    The number, followed by
    if the number is greater than 0: is positive
    if the number is 0: is zero
    if the number is less than 0: is negative
    followed by a new line

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+/* File: 0-positive_or_negative.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - assign a random number to the variable n each time executed
+ * output should be the number followed by:if n is +, - or 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+
+	return (0);
+}
