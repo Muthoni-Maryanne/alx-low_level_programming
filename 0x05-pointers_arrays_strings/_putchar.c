80d47b0daf05c396cba269e6fabad9a5f9e31c5b
Maryanne Muthoni <maryanne.muthoni@student.moringaschool.com>
2024-01-07 21:07:54 -0800

    user defined putchar function added to "main.h" as prototype

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+/* File: _putchar.c file */
+
+#include <stdio.h>
+
+/**
+ * _putchar- user defined putchar function
+ * added to header file as prototype
+ *
+ * Return: 0
+ */
+int _putchar(int ch)
+{
+	putchar(ch);
+	return (0);
+}
