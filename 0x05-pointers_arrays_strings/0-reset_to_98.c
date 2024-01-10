fad1f9f31792989c05bf71d760d5ec55de2cf6a7
Maryanne Muthoni <maryanne.muthoni@student.moringaschool.com>
2024-01-09 23:47:25 -0800

    function that takes a pointer to an int as parameter and updates the value it points to to 98
    pointer &n from main copied into pointer in reset_to_98 function, int *n
    n = &n;
    So now both pointer n in reset_to_98 and pointer &n from main point to variable n = 402
    So value can be changed by pointer n so *n = 98
    Pointer n destroyed after exiting reset_to_98()

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/* File: 0-reset_to_98.c */
+
+#include "main.h"
+
+/**
+ * reset_to_98 -  takes a pointer to an int as parameter
+ * updates the value it points to to 98
+ * @n: pointer which will copy value of pointer from main
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
