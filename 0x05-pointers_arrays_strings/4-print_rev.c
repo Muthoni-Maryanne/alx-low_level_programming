3ceee8d12040b807f1fe21bda2ca9ef3f90db81b
Maryanne Muthoni <107298263+Muthoni-Maryanne@users.noreply.github.com>
2024-01-11 04:53:14 +0300

    function that prints a string, in reverse, followed by a new line
    Prototype: void print_rev(char *s);
    str from main copies value into s
    value of str is the string and also &(str[0])
    To print in reverse use a loop
    Calculate length of string without '\0'
    Create another while loop to print out string values from highhest
    address to lowest

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,16 +14,15 @@ void print_rev(char *s)
 	int index = 0;
 	int length = 0;
 
-	while (s[index] != '\0')
+	while (s[index] <= '\0')
 	{
 		index++;
 		length++;
 	}
 
-	while (index-1 >= 0)
+	while (--index >= 0)
 	{
 		_putchar(s[index]);
-		index--;
 	}
 	_putchar('\n');
 }
