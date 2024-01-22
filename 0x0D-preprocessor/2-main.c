73599a7dbf722c75fcb28b9b6b24f03fe506630c
Maryanne Muthoni <107298263+Muthoni-Maryanne@users.noreply.github.com>
2024-01-22 23:17:45 +0300

    program that prints the name of the file it was compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,6 @@
 /* File: 2-main.c */
 
-#
+#include <stdio.h>
 
 /**
  * main- prints the name of the file it was compiled from
@@ -10,5 +10,9 @@
  */
 int main(void)
 {
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
 	
 
