57b9e7fa1f5a64c741991b9227eca239155065b7
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-01-22 22:45:44 +0300

    Main functions to test header file macro definitions

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+/* File: 2-main.c */
+
+#
+
+/**
+ * main- prints the name of the file it was compiled from
+ * You are allowed to use the standard library
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	
+
