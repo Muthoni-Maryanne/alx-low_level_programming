3ab61c7539c07fc617d24e0b4efe41c7d3a61ac0
Maryanne Muthoni <maryanne.muthoni@student.moringaschool.com>
2024-02-05 12:24:01 -0800

    Define a new type struct dog with the following elements:
    name, type = char *
    age, type = float
    owner, type = char *

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -struct that stores dog information
+ * @name: pointer to string
+ * @age: float
+ * @owner: pointer to string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
