cfa327048e4ccd868b90733223b7bbe66bea41ae
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-02-19 16:13:41 +0300

    function that adds two integers and returns the result
    Prototype: int add(int, int);

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+/* File: 10-add.c */
+
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ * @j: value 1
+ * @k: value 2
+ *
+ * Prototype: int add(int, int);
+ *
+ * Return: int
+ */
+int add(int j, int k)
+{
+	int sum = j + k;
+
+	return (sum);
+}
