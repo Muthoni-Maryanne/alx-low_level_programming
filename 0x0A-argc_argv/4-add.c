5a5f22f0844fc789f788bab0fec1323a8a380365
Maryanne Muthoni <107298263+Muthoni-Maryanne@users.noreply.github.com>
2024-03-22 12:49:20 +0300

    program that adds positive numbers
    Print the result, followed by a new line
    If no number is passed to the program, print 0, followed by a new line
    If one of the number contains symbols that are not digits, print Error, followed by a new line, and return 1
    You can assume that numbers and the addition of all the numbers can be stored in an int

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,33 +15,25 @@
  *
  * Return:0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, sum, index = 0;
+	int i, index, sum = 0;
 
-	if (argc >= 2)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (index = 0; argv[i][index]; index++)
 		{
-			for (index = 0; argv[i][index]; index++)
+			if (argv[i][index] < '0' || argv[i][index] > '9')
 			{
-				if (argv[i][index] < '0' || argv[i][index] > '9')
-				{
-					printf("Error\n");
-					return (1);
-				}
-				else:
-				{
-					sum += atoi(argv[i]);
-				}
-				printf("0\n");
+				printf("Error\n");
+				return (1);
 			}
 		}
+
+		sum += atoi(argv[i]);
 	}
-	else
-	{
-		printf("0");
-	}
+
 	printf("%d\n", sum);
+
 	return (0);
 }
