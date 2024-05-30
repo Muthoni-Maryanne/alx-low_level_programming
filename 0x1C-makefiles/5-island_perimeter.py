76ded5d828cc1a8c983fa102bccbc0c7c9a3ef64
Maryanne Muthoni <maryanne.muthoni@student.moringaschool.com>
2024-05-29 19:59:50 -0700

    i

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
 
 def island_perimeter(grid):
-    """
-    Returns the perimeter of the island described in grid.
-    
+    """Return the perimiter of an island.
+
+    The grid represents water by 0 and land by 1.
+
     Args:
-        grid (list of list of int): A 2D grid where 0 represents water and 1 represents land.
-        
+        grid (list): A list of list of integers representing an island.
     Returns:
-        int: The perimeter of the island.
+        The perimeter of the island defined in grid.
     """
-    rows = len(grid)
-    cols = len(grid[0])
-    perimeter = 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    for r in range(rows):
-        for c in range(cols):
-            if grid[r][c] == 1:
-                # Check all four directions
-                if r == 0 or grid[r-1][c] == 0:  # Check above
-                    perimeter += 1
-                if r == rows-1 or grid[r+1][c] == 0:  # Check below
-                    perimeter += 1
-                if c == 0 or grid[r][c-1] == 0:  # Check left
-                    perimeter += 1
-                if c == cols-1 or grid[r][c+1] == 0:  # Check right
-                    perimeter += 1
-
-    return perimeter
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
